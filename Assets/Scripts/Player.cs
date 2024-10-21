c0966031f9a7d4b3b04fa31d292a35a9b5e15e44
Sahil <issahil404@gmail.com>
2024-10-22 04:29:47 +0530

    refactored codebase

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -18,21 +18,19 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        CalculateMovement();
+    }
+
+
+    void CalculateMovement() {
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
 
-        // transform.Translate(Vector3.right * horizontalInput * _speed * Time.deltaTime);
-        // transform.Translate(Vector3.up * verticalInput * _speed * Time.deltaTime);
+
         Vector3 direction = new Vector3(horizontalInput, verticalInput, 0);
         transform.Translate(direction * _speed * Time.deltaTime);
 
-        // if player position on y > 0
-        // y position = 0
-        if (transform.position.y >= 0) {
-            transform.position = new Vector3(transform.position.x, 0, 0);
-        } else if (transform.position.y < -3.8f) {
-            transform.position = new Vector3(transform.position.x, -3.8f, 0);
-        }
+        transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, -3.8f, 0), 0);
 
         if (transform.position.x > 11.3f) {
             transform.position = new Vector3(-11.3f, transform.position.y, 0);
